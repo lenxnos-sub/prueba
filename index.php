5308684800700d099f257a93168266e50841c417
Nelson Soto <nelson.soto.c05@gmail.com>
2017-07-20 08:37:16 -0430

    iniciando un nuevo proyecto

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Prueba con github</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="myclase">
+		<li><span>li nº 1</span></li>
+		<li><span>li nº 2</span></li>
+		<li><span>li nº 3</span></li>
+	</div>
+</body>
+</html>
